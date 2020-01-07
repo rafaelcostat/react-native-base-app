b96031fcdd7fa30fc3c0708b9fb65d6f23e6d961
Rafael Costa <rafaelcostat@gmail.com>
2020-01-06 23:01:49 -0300

    feat: inital setup

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['error', { allow: ['tron'] }],
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/sort-comp': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello World!</Text>
+      </SafeAreaView>
+    </>
+  );
+}
